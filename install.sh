a2b8a4f0e52ba382e6057d9cb5a5890586ed4466
Niel <dniel150@proton.me>
2026-04-10 14:21:01 -0300

    added more stuff

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ mkdir -p ~/.config
 ln -sf "$DOTFILES_DIR/config/i3" ~/.config/
 ln -sf "$DOTFILES_DIR/config/alacritty" ~/.config/
 ln -sf "$DOTFILES_DIR/config/dunst" ~/.config/
+ln -sf "$DOTFILES_DIR/config/i3status" ~/.config
 
 echo "Running system setup..."
 bash "$DOTFILES_DIR/scripts/setup-system.sh"
