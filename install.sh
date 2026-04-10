0d0c20f45b444a413c69ee3262beadc809a26ebd
Niel <dniel150@proton.me>
2026-04-10 14:51:46 -0300

    added dunst

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ ln -sf "$DOTFILES_DIR/config/i3" ~/.config/
 ln -sf "$DOTFILES_DIR/config/alacritty" ~/.config/
 ln -sf "$DOTFILES_DIR/config/dunst" ~/.config/
 ln -sf "$DOTFILES_DIR/config/i3status" ~/.config
+ln -sf "$DOTFILES_DIR/config/dunst" ~/.config
 
 echo "Running system setup..."
 bash "$DOTFILES_DIR/scripts/setup-system.sh"
